f091242bb484f922cce700d8d88b92653a36d3d3
Honório Gomes <hgunicamp@gmail.com>
2025-08-24 12:35:41 -0300

    Including the Python Virtual Enviroment.

--- bin/delete-python-venv.sh
+++ bin/delete-python-venv.sh
@@ -1,5 +1,6 @@
+
 #! /usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/.env"
 
-[[ -d "${IMAGES_DIR}" ]] && rm -f "${IMAGES_DIR}"/*
+[[ -d "${PYTHON_ENV_DIR}" ]] && rm -rf "${PYTHON_ENV_DIR}"/{bin,lib,CACHEDIR.TAG,pyvenv.cfg}
