3cc1f48d7a21228f4c72e910be3263a773ab64d5
Honório Gomes <hgunicamp@gmail.com>
2025-08-24 14:04:17 -0300

    Fixing error condition on Python Venv.

--- bin/create-python-venv.sh
+++ bin/create-python-venv.sh
@@ -3,9 +3,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/.env"
 
 ${PYTHON_VIRTUAL_ENV} -p ${PYTHON_VERSION} ${PYTHON_ENV_DIR} || \
-  ${PYTHON_EXEC} -m venv ${PYTHON_ENV_DIR} || \
-  echo "Error installing Python Virtual Environment" && \
-  [[ "$?" -gt 0 ]] && exit 1
+  ${PYTHON_EXEC} -m venv ${PYTHON_ENV_DIR}
+
+if [[ "$?" -gt 0 ]]; then
+  echo ""
+  echo "Error installing Python Virtual Environment"
+  echo ""
+  exit 1
+fi
 
 source "${PYTHON_ENV_DIR}"/bin/activate
 pip install -r "${PYTHON_ENV_DIR}"/requirements.txt
